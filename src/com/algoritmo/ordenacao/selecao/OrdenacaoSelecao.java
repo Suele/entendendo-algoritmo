a2b034b651f3ce8c64ce5d62037cb017e7f010ca
suele <suelen.ads.guimaraes@gmail.com>
2021-07-29 23:40:01 -0300

    Adiciona verifica se o array esta vazio.

--- src/com/algoritmo/ordenacao/selecao/OrdenacaoSelecao.java
+++ src/com/algoritmo/ordenacao/selecao/OrdenacaoSelecao.java
@@ -3,17 +3,23 @@ package com.algoritmo.ordenacao.selecao;
 public class OrdenacaoSelecao {
 
     public void buscaMenorElemento (int[] listaElementos) {
-        for (int i = 0; i < listaElementos.length; i++) {
-            int indiceMenor = i;
-            for (int j = i + 1; j < listaElementos.length; j++) {
-                if (listaElementos[j] < listaElementos[indiceMenor]) {
-                    indiceMenor = j;
+        if (listaElementos.length > 1) {
+            for (int i = 0; i < listaElementos.length; i++) {
+                int indiceMenor = i;
+                for (int j = i + 1; j < listaElementos.length; j++) {
+                    if (listaElementos[j] < listaElementos[indiceMenor]) {
+                        indiceMenor = j;
+                    }
+                    int aux = listaElementos[i];
+                    listaElementos[i] = listaElementos[indiceMenor];
+                    listaElementos[indiceMenor] = aux;
                 }
-                int aux = listaElementos[i];
-                listaElementos[i] = listaElementos[indiceMenor];
-                listaElementos[indiceMenor] = aux;
             }
+            for (int f = 0; f < listaElementos.length; f++) {
+                System.out.print(listaElementos[f] + " - ");
+            }
+        } else {
+            System.out.println("O array está vazio ou contem apenas um elemento.");
         }
-        System.out.println(listaElementos[5]);
     }
 }
